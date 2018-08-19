ea42be1bfa2c587276f808d0df1ebf58cd20c10d
Reecon <reecon820@gmail.com>
2018-08-19 20:15:26 +0200

    use specific global vars, move setting class to main file

--- Settings/settings.js
+++ Settings/settings.js
@@ -1,11 +1,1 @@
-﻿var settings = {
-  "Permission": "everyone",
-  "Cooldown": 0.0,
-  "RemoteCooldown": 1,
-  "RemoteCommand": "!q",
-  "RemotePermission": "moderator",
-  "CommandAlt": "!art",
-  "RemoteInfo": "",
-  "Command": "!pet",
-  "Info": ""
-};
+﻿var settings = {"Permission": "everyone", "Cooldown": 0.0, "RemoteCooldown": 1, "RemoteCommand": "!q", "RemotePermission": "moderator", "CommandAlt": "!art", "RemoteInfo": "", "Command": "!pet", "Info": ""};
